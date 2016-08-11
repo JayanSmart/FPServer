7ac20fd2a9ca9133efa5fe88a254b7772ea3f388
Jayan Smart <jayandrinsmart@gmail.com>
2016-08-11 10:43:24 +0200

    created index page, within Django templates convention, and two test questions  --Jayan Smart

--- ProblemFinder/views.py
+++ ProblemFinder/views.py
@@ -1,6 +1,16 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.template import loader
+from .models import Question
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world, your at the index page")
+
+    questions_list = Question.objects.order_by('title')
+    template = loader.get_template('problemfinder/index.html')
+
+    context = {
+        'question_list' : questions_list
+    }
+
+    return HttpResponse(template.render(context, request))
