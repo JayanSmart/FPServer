8164861c08c17826bef6df95cf05e691a48fb533
nicfindlay <nicisme@gmail.com>
2016-08-11 09:52:37 +0200

    Adding Bootstrap

--- ProblemFinder/views.py
+++ ProblemFinder/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world, your at the index page")
+    #return HttpResponse("Hello world, your at the index page")
+    return render(request, "index.html")
