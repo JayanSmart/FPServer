71a63f1b77659e7c4b0a31046f653477065e3862
Jayan Smart <jayandrinsmart@gmail.com>
2016-08-14 16:25:41 +0200

    Made a login link, needs to bu put on the right of the page.  --Jayan Smart

--- ProblemFinder/views.py
+++ ProblemFinder/views.py
@@ -1,16 +1,34 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
+from django.http import Http404
 from .models import Question
 
 
 # Create your views here.
 def index(request):
 
-    questions_list = Question.objects.order_by('title')
-    template = loader.get_template('problemfinder/index.html')
+    #Taking care of the 404 error
+    try:
+        questions_list = Question.objects.order_by('title')
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
 
     context = {
-        'question_list' : questions_list
+        'question_list': questions_list
     }
 
-    return HttpResponse(template.render(context, request))
+    # This is a shortcut and saves having to use the loader class
+    return render(request, "problemfinder/index.html", context)
+
+def bootstrap(request):
+    #Taking care of the 404 error
+    try:
+        questions_list = Question.objects.order_by('title')
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+
+    context = {
+        'question_list': questions_list
+    }
+
+    # This is a shortcut and saves having to use the loader class
+    return render(request, "problemfinder/bootstrap.html", context)
