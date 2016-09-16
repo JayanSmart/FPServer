fe02923a773a7a4cfa60f737c29dcd85fa844c54
Adam Edelberg <EDLADA002@myuct.ac.za>
2016-09-16 14:06:53 +0200

    Search bar changed to form..

--- ProblemFinder/views.py
+++ ProblemFinder/views.py
@@ -1,4 +1,5 @@
 from django.http import Http404
+from django.http import HttpResponse
 from django.shortcuts import render
 
 from .models import Question
@@ -34,3 +35,10 @@ def search(request):
 
     # This is a shortcut and saves having to use the loader class
     return render(request, "problemfinder/search.html", context)
+
+def search(request):
+    if 'q' in request.GET:
+        message = 'You searched for: %r' % request.GET['q']
+    else:
+        message = 'You submitted an empty form.'
+    return HttpResponse(message)
