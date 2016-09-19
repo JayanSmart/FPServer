de11427d124621d923ee2a2e74d89428893a29d4
Adam Edelberg <EDLADA002@myuct.ac.za>
2016-09-19 16:05:18 +0200

    Deleted user form view

--- ProblemFinder/views.py
+++ ProblemFinder/views.py
@@ -7,7 +7,6 @@ from .models import search_alg
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.views.generic import View
-from .forms import UserForm
 import base64
 
 
@@ -118,38 +117,7 @@ def search(request):
 
 
 
-class UserFormView(View):
-    form_class = UserForm
-    template_name = 'problemfinder/login.html'
 
-    def get(self, request):
-        form = self.form_class(None)
-        return render(request, self.template_name, {'form': form})
-
-    def post(self, request):
-        form = AuthenticationForm(data=request.POST)
-        if form.is_valid():
-
-            user = form.save(commit=False)
-
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            print(username+' : '+password   )
-            user.set.password(password)
-            user.save()
-
-            user = authenticate(username=username, password=password)
-            print(user)
-            if user is not None:
-                print(user)
-                if user.is_active:
-                    login(request, user)
-                    return redirect('ProblemFinder:index')
-        else:
-            print("form invalid")
-            print(form.errors)
-
-        return render(request, "problemfinder/login.html")
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
