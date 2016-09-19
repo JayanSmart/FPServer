0de5597cb787c07a0a8a61af14d838836fdf13f3
nicisme <nicisme@gmail.com>
2016-09-19 12:57:43 +0200

    Jayan, everything kinda works (but not really), form invalid

--- ProblemFinder/views.py
+++ ProblemFinder/views.py
@@ -1,7 +1,5 @@
 from django.contrib.auth.forms import AuthenticationForm
-from django.http import Http404, HttpResponse
-from django.shortcuts import render, redirect
-
+from django.http import Http404
 from .models import Question, Solution
 from .models import search_alg
 from django.shortcuts import render, redirect
