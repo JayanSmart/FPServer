95d235189dc08bbc9a947604c7d2b7d3d3346adc
nicisme <nicisme@gmail.com>
2016-09-20 14:51:10 +0200

    Moved search algorithm to views.py (made mistake)

--- ProblemFinder/views.py
+++ ProblemFinder/views.py
@@ -3,7 +3,6 @@ from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 
 from .models import Question, Solution
-from .models import search_alg
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.views.generic import View
